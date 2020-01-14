9fbbf58597d7f9c2f18254d3eba3ff4c919f4347
Adam Westerholm <adam.westerholm@du.edu>
2020-01-14 14:49:25 -0700

    added files for lab03

---
+++ labs/lab03/lab03.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+class lab03 {
+
+  syncFileRead(/* Insert Parameters here */) {
+    /* Implement function here */
+  }
+
+  asyncFileRead(/* Insert Parameters here */) {
+    /* Implement function here */
+  }
+
+  compressFileStream(/* Insert Parameters here */) {
+    /* Implement function here */
+  }
+
+  decompressFileStream(/* Insert Parameters here */) {
+    /* Implement function here */
+  }
+
+  listDirectoryContents(/* Insert Parameters here */) {
+    /* Implement function here */
+  }
+
+}
+
+export {lab03};
